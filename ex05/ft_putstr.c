8b267942b6eb186528eaa80b7e329737ba2b154e
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2021-10-23 19:46:22 +0700

    EX05

--- ex05/ft_putstr.c
+++ ex05/ft_putstr.c
@@ -9,3 +9,17 @@
 /*   Updated: 2021/10/20 23:53:55 by porrapat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+	{
+		ft_putchar(*str++);
+	}
+}
--- ex05/ft_putstr_dev.c
+++ ex05/ft_putstr_dev.c
@@ -9,7 +9,6 @@
 /*   Updated: 2021/10/20 23:53:55 by porrapat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include <unistd.h>
 
 void	ft_putchar(char c)
@@ -17,8 +16,16 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
+void	ft_putstr(char *str)
+{
+	while (*str)
+	{
+		ft_putchar(*str++);
+	}
+}
+
 int	main(void)
 {
-	ft_putchar('c');
+	ft_putstr("Hello");
 	return (0);
 }
