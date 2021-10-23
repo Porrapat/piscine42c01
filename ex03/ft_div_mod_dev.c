7b44ce3a645a69465b0d59a52b71115d02e5e265
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2021-10-23 18:50:56 +0700

    Add Ex03

--- ex03/ft_div_mod_dev.c
+++ ex03/ft_div_mod_dev.c
@@ -9,7 +9,7 @@
 /*   Updated: 2021/10/20 23:53:55 by porrapat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
+#include <stdio.h>
 #include <unistd.h>
 
 void	ft_putchar(char c)
@@ -17,8 +17,42 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		ft_putnbr(nb / 10);
+		ft_putchar('8');
+	}
+	else if (nb < 0)
+	{
+		ft_putchar('-');
+		ft_putnbr(-nb);
+	}
+	else
+	{
+		if (nb > 9)
+		{
+			ft_putnbr(nb / 10);
+		}
+		ft_putchar(48 + nb % 10);
+	}
+}
+
+void	ft_div_mod(int a, int b, int *div, int *mod)
+{
+	*div = a / b;
+	*mod = a % b;
+}
+
 int	main(void)
 {
-	ft_putchar('c');
+	int	div;
+	int	mod;
+
+	ft_div_mod(10, 3, &div, &mod);
+	ft_putnbr(div);
+	ft_putchar('\n');
+	ft_putnbr(mod);
 	return (0);
 }
