bd33f881db28946b0e5357058843e51eb1c4606a
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2021-10-24 12:45:13 +0700

    Add EX07

--- ex07/ft_rev_int_tab.c
+++ ex07/ft_rev_int_tab.c
@@ -9,3 +9,19 @@
 /*   Updated: 2021/10/20 23:53:55 by porrapat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_rev_int_tab(int *tab, int size)
+{
+	int	index;
+	int	tmp;
+
+	index = 0;
+	while (index < size / 2)
+	{
+		tmp = tab[index];
+		tab[index] = tab[size - 1 - index];
+		tab[size - 1 - index] = tmp;
+		index++;
+	}
+}
--- ex07/ft_rev_int_tab_dev.c
+++ ex07/ft_rev_int_tab_dev.c
@@ -9,7 +9,7 @@
 /*   Updated: 2021/10/20 23:53:55 by porrapat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
+#include <stdio.h>
 #include <unistd.h>
 
 void	ft_putchar(char c)
@@ -17,8 +17,56 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
+void	ft_rev_int_tab(int *tab, int size)
+{
+	int	index;
+	int	tmp;
+
+	index = 0;
+	while (index < size / 2)
+	{
+		tmp = tab[index];
+		tab[index] = tab[size - 1 - index];
+		tab[size - 1 - index] = tmp;
+		index++;
+	}
+}
+
+void	debug_dump_array(int numbers[], int size)
+{
+	int	index;
+
+	printf("[ ");
+	index = 0;
+	while (index < size)
+	{
+		printf("%d", numbers[index]);
+		if (index != size - 1)
+		{
+			printf(", ");
+		}
+		index++;
+	}
+	printf(" ]");
+}
+
 int	main(void)
 {
-	ft_putchar('c');
+	int	numbers[9];
+	int	*first_pointer;
+	int	index;
+
+	index = 0;
+	while (index < 9)
+	{
+		numbers[index] = index + 1;
+		index++;
+	}
+	first_pointer = &numbers[0];
+	printf("before: ");
+	debug_dump_array(numbers, 9);
+	ft_rev_int_tab(first_pointer, 9);
+	printf("\nafter : ");
+	debug_dump_array(numbers, 9);
 	return (0);
 }
