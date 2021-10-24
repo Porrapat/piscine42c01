618895d04b3d04714a277a63d2fb705c95623772
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2021-10-24 13:23:02 +0700

    OKFistSubmitWishItOKForNow

--- ex08/ft_sort_int_tab.c
+++ ex08/ft_sort_int_tab.c
@@ -9,3 +9,32 @@
 /*   Updated: 2021/10/20 23:53:55 by porrapat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <stdbool.h>
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int		read_index;
+	int		index;
+	bool	swapped;
+
+	read_index = 0;
+	while (true)
+	{
+		swapped = false;
+		index = 0;
+		while (index < size)
+		{
+			if (tab[index] > tab[index + 1])
+			{
+				ft_swap(&tab[index], &tab[index + 1]);
+				swapped = true;
+			}
+			index++;
+		}
+		if (!swapped)
+		{
+			break ;
+		}
+		read_index++;
+	}
+}
